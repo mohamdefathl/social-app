6e2558d4f224a12df6689c2f704a615cfad4e1f9
mohamdefathl <mohammadefathl@gmail.com>
2024-10-25 22:35:51 +0300

    feat: adding the form , input , button shadcn component

--- src/_auth/forms/sign-up-form.tsx
+++ src/_auth/forms/sign-up-form.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { SignUpSchema } from '@/lib/validations'
 
 const SignUpForm = () => {
-  return (
-    <div>SignUpForm</div>
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
+  function onSubmit(values: z.infer<typeof SignUpSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return ( 
+  <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
   )
 }
 
